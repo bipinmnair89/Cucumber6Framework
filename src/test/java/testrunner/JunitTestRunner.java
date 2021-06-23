588d4635610ab10b437f468d3c1da8f101914063
bipinmnair89 <bipinmnair89@gmail.com>
2021-06-23 11:46:37 +0530

    Parallel running of feature files using Maven failsafe plugin

--- src/test/java/testrunner/JunitTestRunner.java
+++ src/test/java/testrunner/JunitTestRunner.java
@@ -6,17 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/LoginPage.feature",
-                    "src/test/resources/features/MyAccountPage.feature"},
+        features = {"src/test/resources/features"},
         glue = {"steps","hooks"},
         tags = "@login or @accounts",
         plugin={"pretty",
                 "json:target/MyReports/report.json",
                 "junit:target/MyReports/report.xml",
                 "html:target/MyReports/html-report.html",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/"}
 )
 
 
-public class MyRunnerTest {
+public class JunitTestRunner {
 }

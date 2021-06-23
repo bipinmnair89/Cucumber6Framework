c43c0f3151ffabe61b603d4fcf13d20d452f0de8
bipinmnair89 <bipinmnair89@gmail.com>
2021-06-23 15:31:47 +0530

    parallel execution of scenarios using testng runner and maven-surefire-plugin

---
+++ test-output-thread/report.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "aa429d11-a9bc-40e1-b215-51ddb5fa1601",
+    "feature": "My Accounts page feature",
+    "scenario": "Verify links in My Accounts section",
+    "start": 1624441294526,
+    "group": 17,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1624441338663,
+    "className": "passed"
+  },
+  {
+    "id": "f2827029-3436-4848-b38e-044b0da101bc",
+    "feature": "Login page feature",
+    "scenario": "Verify forgot password page",
+    "start": 1624441294526,
+    "group": 14,
+    "content": "",
+    "tags": "@login,",
+    "end": 1624441328348,
+    "className": "passed"
+  },
+  {
+    "id": "e0c5e23b-b3bc-4960-9123-5a33e1c7fc3f",
+    "feature": "My Accounts page feature",
+    "scenario": "Verify links in My Orders section",
+    "start": 1624441294536,
+    "group": 18,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1624441339249,
+    "className": "passed"
+  },
+  {
+    "id": "ced7f584-e9ea-4c9d-b80f-38d9c5db04d4",
+    "feature": "Login page feature",
+    "scenario": "Verify register new user page",
+    "start": 1624441294430,
+    "group": 15,
+    "content": "",
+    "tags": "@login,",
+    "end": 1624441331470,
+    "className": "passed"
+  },
+  {
+    "id": "bf150081-0bf8-4fb9-93bd-7ed609eb3375",
+    "feature": "Login page feature",
+    "scenario": "Login into application",
+    "start": 1624441294536,
+    "group": 16,
+    "content": "",
+    "tags": "@login,",
+    "end": 1624441339271,
+    "className": "passed"
+  },
+  {
+    "id": "119c9309-6a56-4265-9ff8-9ff0c3b02021",
+    "feature": "My Accounts page feature",
+    "scenario": "Dummy failure scenario test",
+    "start": 1624441294526,
+    "group": 19,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1624441339133,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
+  }
+]);
+});
